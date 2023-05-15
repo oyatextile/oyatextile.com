41ab389a884dffb8acd12e361d404b4e53171a24
OYATETXILE <118007189+oyatextile@users.noreply.github.com>
2023-05-15 15:38:04 +0100

    add color

--- pages/products/pet.tsx
+++ pages/products/pet.tsx
@@ -4,8 +4,8 @@ import Head from "next/head";
 import React from "react";
 import SeoTags from "../../components/seoTags";
 import TabsCategory from "../../components/TabsCategory";
-import { Link } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import client, {
   getAllproductByPage,
   getSeoForPate,
@@ -68,9 +68,24 @@ const HeadCat = () => {
       >
         {/* Subscribe to our newsletter<a href="https://www.linkedin.com/newsletters/pet-textile-news-6897475199600615424/" target="_blank">&nbsp;
         <strong> on LINKEDIN </strong>&nbsp;</a> for more information and tips.  */}
-
-        Subscribe to our newsletter  <Link href="https://www.linkedin.com/newsletters/pet-textile-news-6897475199600615424/" isExternal>on LINKEDIN {" "}  <ExternalLinkIcon mx='2px' /></Link> or {" "}
-        <Link href="https://oyatextile.com/cms/wp-content/uploads/2023/05/OyaPet-.pdf" isExternal> {" "}download our Catalog  <ExternalLinkIcon mx='2px' /></Link> for more information and tips.
+        Subscribe to our newsletter{" "}
+        <Link
+          href="https://www.linkedin.com/newsletters/pet-textile-news-6897475199600615424/"
+          color="#3ea394"
+          isExternal
+        >
+          on LINKEDIN <ExternalLinkIcon mx="2px" />
+        </Link>{" "}
+        or{" "}
+        <Link
+          color="#3ea394"
+          href="https://oyatextile.com/cms/wp-content/uploads/2023/05/OyaPet-.pdf"
+          isExternal
+        >
+          {" "}
+          download our Catalog <ExternalLinkIcon mx="2px" />
+        </Link>{" "}
+        for more information and tips.
       </Text>
     </Box>
   );
